a1160164d6650f7a1570327452402366ea94b776
yuangao0118 <yug216@lehigh.edu>
2016-02-15 04:54:46 +0000

    updated Convert

--- hw03/Convert.java
+++ hw03/Convert.java
@@ -1,15 +1,18 @@
-// Convert Meter to inches
+//HW03 Convert
+//Yuan Gao
+//yug216
+//Convert Meter to inches
 // input meter and convert to inches
-  import java.util.Scanner;
+  import java.util.Scanner; //first line, import scanner
   public class Convert{
       public static void main(String[] args){
-          Scanner MyScanner = new Scanner(System.in);
+          Scanner MyScanner = new Scanner(System.in); //define scanner
           System.out.print ("Enter the distance in meters: ");
           double DistanceInMeter=MyScanner.nextDouble();
           double MetertoInches; //meter to inches converison
-          MetertoInches=39.37;
-          double DistanceInInch;
-          DistanceInInch=MetertoInches*DistanceInMeter;
+          MetertoInches=39.3701; //conversion
+          double DistanceInInch=0;
+          DistanceInInch=MetertoInches*DistanceInMeter*100/100;
           System.out.println(DistanceInMeter + " meters is " + DistanceInInch + " Inches.");
           
       }
--- hw03/Pyramid.java
+++ hw03/Pyramid.java
@@ -1,15 +1,18 @@
+//HW03 Calculate Pyramid volume
+//Yuan Gao
+//yug216
 // input square side of Pyramid and height of Pyramid
 // calculate the volume 
-  import java.util.Scanner;
+  import java.util.Scanner; //import Scanner
   public class Pyramid{
   public static void main(String[] args){
       Scanner MyScanner = new Scanner(System.in);
-      System.out.print ("The Square side of Pyramid is: ");
+      System.out.print ("The Square side of Pyramid is: "); //ask for input
       double SquareSide=MyScanner.nextDouble();
-      System.out.print ("The height of the Pyramid is: ");
+      System.out.print ("The height of the Pyramid is: "); //ask for input
       double Height=MyScanner.nextDouble();
       double Volume;
-      Volume=SquareSide*SquareSide*Height/3;
+      Volume=SquareSide*SquareSide*Height/3; //function
       System.out.println("The Volume of this Pyramid is: "+ Volume);
       
       }    
